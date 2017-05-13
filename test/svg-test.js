78f54bc2d19e3eab4e0e4e2eb814a5c07dcfa217
Kiernan McGowan <timmcgowan625@gmail.com>
2017-05-12 20:06:15 -0500

    fix test

--- test/svg-test.js
+++ test/svg-test.js
@@ -4,8 +4,8 @@ var tape = require('tape-catch'),
     d3_svg = require('../');
 
 tape('d3_svg creates an svg element', function(test) {
-  var document = jsdom.jsdom();
-  global.document = document;
+  var dom = new jsdom.JSDOM();
+  global.document = dom.window.document;
   var svg = d3_svg.create('body');
   test.ok(svg, 'svg element exists');
   delete global.document;
@@ -13,8 +13,8 @@ tape('d3_svg creates an svg element', function(test) {
 });
 
 tape('d3_svg can set height and with on svg element', function(test) {
-  var document = jsdom.jsdom();
-  global.document = document;
+  var dom = new jsdom.JSDOM();
+  global.document = dom.window.document;
   var svg = d3_svg.create('body', {width: 100, height: 50});
   test.ok(svg, 'svg element exists');
   test.ok(svg.attr('width', 100), 'svg element has correct width');
@@ -24,8 +24,8 @@ tape('d3_svg can set height and with on svg element', function(test) {
 });
 
 tape('d3_svg creates an svg element that can be appended to', function(test) {
-  var document = jsdom.jsdom();
-  global.document = document;
+  var dom = new jsdom.JSDOM();
+  global.document = dom.window.document;
   var svg = d3_svg.create('body');
   var g = svg.append('g');
   test.ok(g, 'g element exists');
